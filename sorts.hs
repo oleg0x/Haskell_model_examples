1cec0eadcc9b9016ba8bc1c21be3b7a5eb0bc694
Oleg <64661077+oleg0x@users.noreply.github.com>
2022-01-06 14:44:42 +0300

    Add files via upload

--- sorts.hs
+++ sorts.hs
@@ -9,8 +9,23 @@ insertionSort (x : xs) = insSort x (insertionSort xs)
 
 
 
-qsort []     = []
-qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
+quickSort [] = []
+quickSort (x:xs) = quickSort smaller ++ [x] ++ quickSort larger
 	where
 		smaller = [a | a <- xs, a <= x]
 		larger  = [b | b <- xs, b > x]
+
+
+
+merge xs [] = xs
+merge [] ys = ys
+merge (x : xs) (y : ys) =
+	if x < y then x : merge xs (y : ys) else y : merge (x : xs) ys
+
+halve :: [a] -> ([a], [a])
+halve xs = splitAt (length xs `div` 2) xs
+
+mergeSort []  = []
+mergeSort [x] = [x]
+mergeSort xs = merge (mergeSort ys) (mergeSort zs)
+	where (ys, zs) = halve xs
